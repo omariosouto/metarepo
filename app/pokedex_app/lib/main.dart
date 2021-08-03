65e6604b3ec71bef16d10f8ed4c1315acf756980
Mario Souto <mario.souto@nubank.com.br>
2021-08-02 23:57:49 -0300

    data fetched with success

--- app/pokedex_app/lib/main.dart
+++ app/pokedex_app/lib/main.dart
@@ -1,4 +1,62 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
+
+class Pokemon {
+  final String id;
+  final String name;
+  final String image;
+
+  Pokemon({
+    required this.id,
+    required this.name,
+    required this.image,
+  });
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) {
+    return Pokemon(
+      id: json['id'],
+      name: json['name'],
+      image: json['image'],
+    );
+  }
+}
+
+Future<List<Pokemon>> fetchPokemon() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  final apiResponse = await http.post(
+    Uri.parse('https://metarepo.vercel.app/api/graphql'),
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode({
+      'query': '''
+        query {
+          pokemons {
+            id
+            name
+            image
+          }
+        }
+      ''',
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    final pokemonsFromServer =
+        jsonDecode(apiResponse.body)['data']['pokemons'] as List<dynamic>;
+    return pokemonsFromServer
+        .map((pokemon) => Pokemon.fromJson(pokemon))
+        .toList();
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+// ===========================================
 
 void main() {
   runApp(MyApp());
@@ -27,12 +85,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 20;
+  late Future<List<Pokemon>> futureAlbum;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchPokemon();
   }
 
   @override
@@ -41,24 +99,41 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'Initial amount of pokemons supported',
-            ),
-            Text(
-              '[$_counter]',
-              style: Theme.of(context).textTheme.headline4,
+            FutureBuilder<List<Pokemon>>(
+              future: futureAlbum,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: snapshot.data!
+                        .map((pokemon) => Row(
+                              children: <Widget>[
+                                Text(pokemon.id),
+                                Image.network(
+                                  pokemon.image,
+                                  width: 200,
+                                  height: 200,
+                                  fit: BoxFit.contain,
+                                ),
+                                Text(capitalize(pokemon.name)),
+                              ],
+                            ))
+                        .toList(),
+                  );
+                }
+
+                if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+
+                return const CircularProgressIndicator();
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
